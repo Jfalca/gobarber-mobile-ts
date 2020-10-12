e893cb05cc4121622dd5790815e6244486214671
Jorge Falca <jfalca@live.com>
2020-10-11 21:24:53 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => <View style={{ backgroundColor: '#312e38' }} />;
+
+export default App;
